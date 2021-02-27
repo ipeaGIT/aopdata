868c1ed79effa74d2cd52987b636a43a394fa50a
rafapereirabr <rafa.pereira.br@gmail.com>
2021-02-27 15:19:27 -0300

    fix global variable

--- r-package/R/zzz.R
+++ r-package/R/zzz.R
@@ -1,20 +1,18 @@
 # nocov start
-utils::globalVariables(c(".", ":="))
-
 .onLoad = function(lib, pkg) {
   requireNamespace("sf")
   requireNamespace("data.table")
 }
 
 
-#' @importFrom data.table setDT :=
+#' @importFrom data.table setDT := %like%
 #' @importFrom utils head tail
 NULL
 
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1") utils::globalVariables(
-  c('geometry', 'i.geom', 'showProgress', 'type'))
+  c('geometry', 'i.geom', 'showProgress', 'type', '.', ':=', '%like%'))
 
 
 
