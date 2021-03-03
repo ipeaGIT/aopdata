50b2d5a706ab136534a787051bdf4fada9e45078
Daniel Herszenhut <dhersz@gmail.com>
2021-03-03 10:27:00 -0300

    removes requireNamespace calls from .onLoad

--- r-package/R/zzz.R
+++ r-package/R/zzz.R
@@ -1,10 +1,3 @@
-# nocov start
-.onLoad = function(lib, pkg) {
-  requireNamespace("sf")
-  requireNamespace("data.table")
-}
-
-
 #' @importFrom data.table setDT := %like%
 #' @importFrom utils head tail
 NULL
@@ -13,8 +6,3 @@ NULL
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1") utils::globalVariables(
   c('geometry', 'i.geom', 'showProgress', 'type', '.', ':=', '%like%'))
-
-
-
-
-# nocov end
