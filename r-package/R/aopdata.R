cfcfbe33b9999ef09281351b8b7c4b2a082dccd6
rafapereirabr <rafa.pereira.br@gmail.com>
2022-02-07 12:40:47 -0300

    update documentation

---
+++ r-package/R/aopdata.R
@@ -0,0 +1,24 @@
+#' aopdata: Data from the 'Access to Opportunities Project (AOP)'
+#'
+#' Download data from the 'Access to Opportunities Project (AOP)'. The 'aopdata'
+#' package brings annual estimates of access to employment, health and education
+#' services by transport mode, as well as data on the spatial distribution of
+#' population, schools and health-care facilities at a fine spatial resolution
+#' for all cities included in the study. More info on the 'AOP' website <https://www.ipea.gov.br/acessooportunidades/en/>.
+#'
+#' @section Usage:
+#' Please check the vignettes on the [website](https://ipeagit.github.io/aopdata/).
+#'
+#' @docType package
+#' @name aopdata
+#' @aliases aopdata-package
+#'
+#' @importFrom data.table setDT := %like%
+#' @importFrom utils globalVariables head tail
+"_PACKAGE"
+
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1") utils::globalVariables(
+  c('geometry', 'i.geom', 'showProgress', 'type', '.', ':=', '%like%'))
+
--- r-package/R/zzz.R
+++
@@ -1,8 +0,0 @@
-#' @importFrom data.table setDT := %like%
-#' @importFrom utils head tail
-NULL
-
-
-## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1") utils::globalVariables(
-  c('geometry', 'i.geom', 'showProgress', 'type', '.', ':=', '%like%'))
